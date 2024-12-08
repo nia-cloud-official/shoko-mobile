7e377f8f6668c9de95dd4585c17f34c50dbe545c
Milton Vafana <146335315+nia-cloud-official@users.noreply.github.com>
2024-12-08 18:50:17 +0200

    modified:   package.json 	modified:   src/consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Bould';
-export const SITE_DESCRIPTION = 'Blogs, redesigned';
+export const SITE_TITLE = 'Shoko Mobile';
+export const SITE_DESCRIPTION ='Zimbabwe News';
